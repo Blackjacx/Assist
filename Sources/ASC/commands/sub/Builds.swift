d712e265bf6d1b506434131e927a0dda4e130bc8
Stefan Herold <stefan.herold@gmail.com>
2021-02-08 00:32:57 +0100

    Implement listing builds
    As usual the list can be restricted by filters

---
+++ Sources/ASC/commands/sub/Builds.swift
@@ -0,0 +1,50 @@
+//
+//  Builds.swift
+//  ASC
+//
+//  Created by Stefan Herold on 04.02.20.
+//
+
+import Foundation
+import ASCKit
+import ArgumentParser
+
+extension ASC {
+
+    struct Builds: ParsableCommand {
+        static var configuration = CommandConfiguration(
+            abstract: "Manage builds for testers and submit builds for review.",
+            subcommands: [List.self],
+            defaultSubcommand: List.self)
+    }
+}
+
+extension ASC.Builds {
+
+    /// Find and list builds for all apps in App Store Connect.
+    /// https://developer.apple.com/documentation/appstoreconnectapi/list_builds
+    struct List: ParsableCommand {
+
+        static var configuration = CommandConfiguration(abstract: "Find and list builds for all apps in App Store Connect.")
+
+        // The `@OptionGroup` attribute includes the flags, options, and arguments defined by another
+        // `ParsableArguments` type.
+        @OptionGroup()
+        var options: Options
+
+        @Option(name: .shortAndLong, help: "Filter which is set as part of the request. See https://developer.apple.com/documentation/appstoreconnectapi/list_builds for possible values.")
+        var filters: [Filter] = []
+
+        @Option(name: .shortAndLong, help: "Number of resources to return.")
+        var limit: UInt = ASCKit.Constants.pagingLimit
+
+        @Argument(help: "The attribute you want to get [expired, minOsVersion, processingState, version, usesNonExemptEncryption, uploadedDate, expirationDate] (default: id).")
+        var attribute: String?
+
+        func run() throws {
+            let op = ListBuildsOperation(filters: filters, limit: limit)
+            op.executeSync()
+            try op.result.get().out(attribute)
+        }
+    }
+}
