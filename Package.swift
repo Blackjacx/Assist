6301136c31482026b312c6d2502eb3e8849d78fd
Stefan Herold <stefan.herold@gmail.com>
2021-07-04 16:59:56 +0200

    Re-implement Commands using async / await
    - Use newest OS/Swift versions
    - Use async branch from swift-argument-parser
    - re-implement all commands to be async
    - replace main.swift by @main

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "Assist",
     platforms: [
-        .macOS(.v11),
-//        .iOS(.v11),
-//        .watchOS(.v5),
-//        .tvOS(.v11)
+      .macOS(.v12)
     ],
     products: [
         .executable(name: "asc", targets: ["ASC"]),
@@ -15,13 +12,13 @@ let package = Package(
         .executable(name: "snap", targets: ["Snap"])
     ],
     dependencies: [
-        .package(name: "ASCKit", url: "https://github.com/blackjacx/asckit", .branch("develop")),
-        .package(name: "Engine", url: "https://github.com/blackjacx/Engine", .branch("develop")),
-//        .package(name: "ASCKit", path: "../ASCKit"),
-//        .package(name: "Engine", path: "../Engine"),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/vapor/jwt-kit.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/kareman/SwiftShell", .upToNextMajor(from: "5.1.0"))        
+//        .package(name: "ASCKit", url: "https://github.com/blackjacx/asckit", .branch("develop")),
+//        .package(name: "Engine", url: "https://github.com/blackjacx/Engine", .branch("develop")),
+        .package(name: "ASCKit", path: "../ASCKit"),
+        .package(name: "Engine", path: "../Engine"),
+        .package(url: "https://github.com/apple/swift-argument-parser", branch: "async"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.1.0"),
+        .package(url: "https://github.com/kareman/SwiftShell", from: "5.1.0")
     ],
     targets: [
         .target(
--- Sources/ASC/main.swift
+++
@@ -1,2 +0,0 @@
-let args = Array(CommandLine.arguments.dropFirst()) // drops script path itself
-ASC.main(args)
