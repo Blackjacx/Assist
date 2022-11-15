db4dd3c24fc265748b6b50f13273199d45b941e4
Stefan Herold <stefan.herold@gmail.com>
2022-11-15 14:03:59 +0100

    Switch to String types when specifying devices
    The removes the pain to always add new device types to the enum when
    Apple releases a new Xcode 😅

--- Sources/Core/Shell/Simctl/Simctl.swift
+++ Sources/Core/Shell/Simctl/Simctl.swift
@@ -47,13 +47,13 @@ public extension Simctl {
         return devices
     }
 
-    static func deviceIdsFor(deviceTypes: [DeviceType], runtime: Runtime) throws -> [String] {
+    static func deviceIdsFor(deviceNames: [String], runtime: Runtime) throws -> [String] {
         var deviceIDs: [String] = []
 
-        try deviceTypes.forEach { (deviceType) in
-            guard let deviceID = try devicesForRuntime(runtime).first(where: { $0.name == deviceType.simCtlValue }) else {
+        try deviceNames.forEach { (name) in
+            guard let deviceID = try devicesForRuntime(runtime).first(where: { $0.name == name }) else {
                 // Create device if it is not yet available
-                let deviceID = try createDevice(name: deviceType.simCtlValue, id: deviceType.simCtlValue, runtime: runtime)
+                let deviceID = try createDevice(name: name, id: name, runtime: runtime)
                 deviceIDs.append(deviceID)
                 return
             }
@@ -177,85 +177,6 @@ public extension Simctl {
         case dark
 
         public var parameterName: String { rawValue }
-    }
-
-//    "Apple TV 4K (2nd generation)"
-//    "Apple TV 4K (at 1080p) (2nd generation)"
-//    "Apple TV 4K (at 1080p)"
-//    "Apple TV 4K"
-//    "Apple TV"
-//    "Apple Watch SE - 40mm"
-//    "Apple Watch SE - 44mm"
-//    "Apple Watch Series 3 - 38mm"
-//    "Apple Watch Series 3 - 42mm"
-//    "Apple Watch Series 4 - 40mm"
-//    "Apple Watch Series 4 - 44mm"
-//    "Apple Watch Series 5 - 40mm"
-//    "Apple Watch Series 5 - 44mm"
-//    "Apple Watch Series 6 - 40mm"
-//    "Apple Watch Series 6 - 44mm"
-//    "iPad (5th generation)"
-//    "iPad (6th generation)"
-//    "iPad (7th generation)"
-//    "iPad (8th generation)"
-//    "iPad Air (3rd generation)"
-//    "iPad Air (4th generation)"
-//    "iPad Air 2"
-//    "iPad Air"
-//    "iPad Pro (10.5-inch)"
-//    "iPad Pro (11-inch) (1st generation)"
-//    "iPad Pro (11-inch) (2nd generation)"
-//    "iPad Pro (11-inch) (3rd generation)"
-//    "iPad Pro (12.9-inch) (1st generation)"
-//    "iPad Pro (12.9-inch) (2nd generation)"
-//    "iPad Pro (12.9-inch) (3rd generation)"
-//    "iPad Pro (12.9-inch) (4th generation)"
-//    "iPad Pro (12.9-inch) (5th generation)"
-//    "iPad Pro (9.7-inch)"
-//    "iPad mini (5th generation)"
-//    "iPad mini 2"
-//    "iPad mini 3"
-//    "iPad mini 4"
-//    "iPhone 8 Plus"
-//    "iPhone 8"
-
-    enum DeviceType: String, CaseIterable {
-        // DEPRECATED: Use `iPhoneSE2` instead
-        case iPhoneSE
-        case iPhoneSE2
-        case iPhoneSE3
-        case iPhone11
-        case iPhone11Pro
-        case iPhone11ProMax
-        case iPhone12Mini
-        case iPhone12
-        case iPhone12Pro
-        case iPhone12ProMax
-        case iPhone13Mini
-        case iPhone13
-        case iPhone13Pro
-        case iPhone13ProMax
-
-        public var parameterName: String { rawValue }
-
-        public var simCtlValue: String {
-            switch self {
-            case .iPhoneSE: return "iPhone SE (2nd generation)"
-            case .iPhoneSE2: return "iPhone SE (2nd generation)"
-            case .iPhoneSE3: return "iPhone SE (3rd generation)"
-            case .iPhone11: return "iPhone 11"
-            case .iPhone11Pro: return "iPhone 11 Pro"
-            case .iPhone11ProMax: return "iPhone 11 Pro Max"
-            case .iPhone12Mini: return "iPhone 12 mini"
-            case .iPhone12: return "iPhone 12"
-            case .iPhone12Pro: return "iPhone 12 Pro"
-            case .iPhone12ProMax: return "iPhone 12 Pro Max"
-            case .iPhone13Mini: return "iPhone 13 mini"
-            case .iPhone13: return "iPhone 13"
-            case .iPhone13Pro: return "iPhone 13 Pro"
-            case .iPhone13ProMax: return "iPhone 13 Pro Max"
-            }
-        }
     }
 
     /// This enum represents only the latest iOS versions from a major version.
