7f9544909a5bddb9175fb4cd6b25991c8e9ac150
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2025-03-25 15:18:27 +0100

    Release version `0.8.0`

--- Sources/Core/Core.swift
+++ Sources/Core/Core.swift
@@ -1,12 +1,5 @@
-//
-//  Core.swift
-//  Core
-//
-//  Created by Stefan Herold on 24.02.20.
-//
-
 import Foundation
 
-public struct Constants {
-    public static let version = "0.7.0"
+public enum Constants {
+    public static let version = "0.8.0"
 }
