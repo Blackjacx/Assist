ce589e853c73b8bf0299980983d071afdccd2b6c
Stefan Herold <794372+Blackjacx@users.noreply.github.com>
2025-10-23 12:35:24 +0200

    chore: remove not needed platforms from Package.swift

--- Package.swift
+++ Package.swift
@@ -3,12 +3,8 @@ import PackageDescription
 
 let package = Package(
     name: "Assist",
-//    platforms: [.macOS(.v10_15), .macCatalyst(.v13), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     platforms: [
         .macOS(.v13)
-//        .iOS(.v11),
-//        .watchOS(.v5),
-//        .tvOS(.v11),
     ],
     products: [
         .executable(name: "asc", targets: ["ASC"]),
